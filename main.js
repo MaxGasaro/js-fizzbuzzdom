fec2747f36fa348bcc70a722207924f803b0d325
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-01-22 16:05:52 +0100

    inizializzazione progetto con ciclo for

---
+++ main.js
@@ -0,0 +1,10 @@
+let quadrato = document.getElementById('container');
+// quadrato.innerHTML = 'ciao';
+
+for (let i = 0; i <= 50; i++) {
+    quadrato.innerHTML += '<div class="box">' + i + '</div>'
+}
+
+// if ( 3 = 0) {
+//     quadrato.innerHTML += '<div class="col"><div class="box-green"></div></div>'
+// }
